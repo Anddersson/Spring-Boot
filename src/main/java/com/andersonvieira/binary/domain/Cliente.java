f6204b994bf1a150d81c9070b8abe1843fc63c65
Anddersson <andvdasilva@gmail.com>
2019-06-28 02:23:11 -0300

    pedido, EstadoPagamento, pagamento

--- src/main/java/com/andersonvieira/binary/domain/Cliente.java
+++ src/main/java/com/andersonvieira/binary/domain/Cliente.java
@@ -29,6 +29,8 @@ public class Cliente implements Serializable{
 	private String CpfOuCnpj;
 	private Integer tipo;
 	
+	
+	
 	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
@@ -36,6 +38,9 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 		
@@ -105,7 +110,15 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
 
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -130,5 +143,6 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
+
+
 }
