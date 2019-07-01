88bf8a17effb670e865e7f2f7e1097921ad225c0
Anddersson <andvdasilva@gmail.com>
2019-07-01 02:34:23 -0300

    Bloqueio de email repetido

--- src/main/java/com/andersonvieira/binary/repositories/ClienteRepository.java
+++ src/main/java/com/andersonvieira/binary/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.andersonvieira.binary.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,8 @@ import com.andersonvieira.binary.domain.Cliente;
 
 @Repository
 public interface  ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional
+	Cliente findByEmail(String email);
 }
+
