5a8d5f7397b1df5734a8d40cbdb52b22e7b6725e
Anddersson <andvdasilva@gmail.com>
2019-09-18 16:13:12 -0300

    atualizando backend

--- src/main/java/com/andersonvieira/binary/resources/ProdutoResource.java
+++ src/main/java/com/andersonvieira/binary/resources/ProdutoResource.java
@@ -1,25 +1,18 @@
 package com.andersonvieira.binary.resources;
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andersonvieira.binary.domain.Produto;
 import com.andersonvieira.binary.dto.ProdutoDTO;
-import com.andersonvieira.binary.dto.ProdutoNewDTO;
+import com.andersonvieira.binary.resources.utils.URL;
 import com.andersonvieira.binary.services.ProdutoService;
 
 @RestController
@@ -27,56 +20,27 @@ import com.andersonvieira.binary.services.ProdutoService;
 public class ProdutoResource {
 	
 	@Autowired
-	private ProdutoService produtoService;
+	private ProdutoService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Produto> find(@PathVariable Integer id) {
-		Produto obj = produtoService.find(id);
+		Produto obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody ProdutoNewDTO objDto) {
-		Produto obj = produtoService.fromDTO(objDto);
-		obj = produtoService.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-	
-	}
-	
-	@PreAuthorize("hasAnyRole('ADMIN')")
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@Valid @RequestBody ProdutoDTO objDto, @PathVariable Integer id) {
-		Produto obj = produtoService.fromDTO(objDto);
-		obj.setId(id);
-		obj = produtoService.update(obj);
-		return ResponseEntity.noContent().build();
-	}
-	
-	@PreAuthorize("hasAnyRole('ADMIN')")
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable Integer id) {
-		produtoService.delete(id);
-		return ResponseEntity.noContent().build();
-	}
-	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<ProdutoDTO>> findAll() {
-		List<Produto> list = produtoService.findAll();
-		List<ProdutoDTO> listDto = list.stream().map(obj -> new ProdutoDTO(obj)).collect(Collectors.toList());  
-		return ResponseEntity.ok().body(listDto);
-	}
-	
-	@RequestMapping(value="/page", method=RequestMethod.GET)
 	public ResponseEntity<Page<ProdutoDTO>> findPage(
+			@RequestParam(value="nome", defaultValue="") String nome, 
+			@RequestParam(value="categorias", defaultValue="") String categorias, 
 			@RequestParam(value="page", defaultValue="0") Integer page, 
 			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
 			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
 			@RequestParam(value="direction", defaultValue="ASC") String direction) {
-		Page<Produto> list = produtoService.findPage(page, linesPerPage, orderBy, direction);
+		String nomeDecoded = URL.decodeParam(nome);
+		List<Integer> ids = URL.decodeIntList(categorias);
+		Page<Produto> list = service.search(nomeDecoded, ids, page, linesPerPage, orderBy, direction);
 		Page<ProdutoDTO> listDto = list.map(obj -> new ProdutoDTO(obj));  
 		return ResponseEntity.ok().body(listDto);
 	}
-	
+
 }
--- src/main/java/com/andersonvieira/binary/services/ProdutoService.java
+++ src/main/java/com/andersonvieira/binary/services/ProdutoService.java
@@ -1,38 +1,30 @@
 package com.andersonvieira.binary.services;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.andersonvieira.binary.domain.Categoria;
 import com.andersonvieira.binary.domain.Produto;
-import com.andersonvieira.binary.domain.enums.Perfil;
-import com.andersonvieira.binary.dto.ProdutoDTO;
-import com.andersonvieira.binary.dto.ProdutoNewDTO;
+import com.andersonvieira.binary.repositories.CategoriaRepository;
 import com.andersonvieira.binary.repositories.ProdutoRepository2;
-import com.andersonvieira.binary.security.UserSS;
-import com.andersonvieira.binary.services.exceptions.AuthorizationException;
-import com.andersonvieira.binary.services.exceptions.DataIntegrityException;
 import com.andersonvieira.binary.services.exceptions.ObjectNotFoundException;
-
 
 @Service
 public class ProdutoService {
 	
-		
 	@Autowired
 	private ProdutoRepository2 repo;
 	
-		
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public Produto find(Integer id) {
-		UserSS user = UserService.authenticated();
-		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
-			throw new AuthorizationException("Acesso negado");
-		}
 		Produto obj = repo.findOne(id);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
@@ -40,53 +32,12 @@ public class ProdutoService {
 		}
 		return obj;
 	}
-	
-	public Produto insert(Produto obj) {
-		obj.setId(null);
-		obj = repo.save(obj);
-		return obj;
-	}
-	
 
-	public Produto update(Produto obj) {
-		Produto newObj = find(obj.getId());
-		updateData(newObj, obj);
-		return repo.save(newObj);
-	}
-
-	public void delete(Integer id) {
-		find(id);
-		try {
-			repo.delete(id);
-		}
-		catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir");
-		}
-	}
-	
-	public List<Produto> findAll() {
-		return repo.findAll();
-	}
-	
-	public Page<Produto> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		return repo.findAll(pageRequest);
+		List<Categoria> categorias = categoriaRepository.findAll(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
 	}
-	
-	public Produto fromDTO(ProdutoDTO objDto) {
-		return new Produto(objDto.getId(), objDto.getNome(),objDto.getPreco());
-	}
-	
-	public Produto fromDTO(ProdutoNewDTO objDto) {
-		Produto p = new Produto(null, objDto.getNome(), objDto.getPreco());
-		return p;
-	}
-	
-	private void updateData (Produto newObj, Produto obj) {
-		newObj.setNome(obj.getNome());
-		newObj.setPreco(obj.getPreco());
-	}
-
 }
 
 
